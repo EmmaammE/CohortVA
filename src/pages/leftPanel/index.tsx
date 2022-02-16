8d208fa1f5ea04ec66acefd5669ba2b24e332732
zrc <zrcccrz@outlook.com>
2022-02-16 13:25:23 +0800

    feat: toggle search view

--- src/pages/leftPanel/index.tsx
+++ src/pages/leftPanel/index.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import CohortSearcherPanel from './CohortSearch';
 import './index.scss';
 
-const LeftPanel = () => (
-  <div id="left-panel">
-    <div className="figures-view g-divider panel">
-      <h2 className="g-title">Specify Initial Figures</h2>
-    </div>
+const LeftPanel = () => {
+  const [show, setShow] = useState(true);
 
-    <div className="identification-view panel">
-      <h2 className="g-title">Cohort Identification View</h2>
-      <div className="iv--content">
-        <div>
-          <h3 className="g-title">Cohort Analysis Provenance</h3>
+  const toggleShow = useCallback(() => {
+    setShow(!show);
+  }, [show]);
+
+  return (
+    <div id="left-panel">
+      <div className="figures-view g-divider panel">
+        <div className="figures-view--header">
+          <h2 className="g-title">Specify Initial Figures</h2>
+          <div className="figures-view--icon" onClick={toggleShow} />
         </div>
-        <div>
-          <h3 className="g-title">Cohort Feature Overview</h3>
+        {show && <CohortSearcherPanel />}
+      </div>
+
+      <div className="identification-view panel">
+        <h2 className="g-title">Cohort Identification View</h2>
+        <div className="iv--content">
+          <div>
+            <h3 className="g-title">Cohort Analysis Provenance</h3>
+          </div>
+          <div>
+            <h3 className="g-title">Cohort Feature Overview</h3>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default LeftPanel;
